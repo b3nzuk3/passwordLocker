7b29e6c9839013a16d327a4cb0d67e7b0cd7aeb0
b3nzuk3 <hussenito7@gmail.com>
2022-04-28 20:35:08 +0300

    adding doc strings to my methods

--- username.py
+++ username.py
@@ -6,17 +6,32 @@ class User_name:
     users = []
 
     def __init__(self):
+        """
+        first we make instances of the variables
+        """
         self.user_name = None
         self.pass_word = None
 
     def save_user(self,user):
+        """
+        this method allows the ability to save user in a list
+        """
+
+
         User_name.users.append(user)
 
     def save_account(self, site, password):
+
+        """
+        allows user to save account credentials once created
+        """
         with open("accounts.txt", "a+") as f:
             f.write("Website: " + site + " "+ " Password: " + password +"\n")
 
     def del_account(self,site):
+        """
+        method to facilitate deletion of credentials in the future
+        """
 
         with open("accounts.txt", "r") as f:
             lines = f.readlines()
@@ -29,6 +44,9 @@ class User_name:
         return line
 
     def display_contact(self):
+        """
+        method allows the displaying of user credentials saves in a file 
+        """
 
         with open("accounts.txt", "r") as f:
             lines = f.read()
